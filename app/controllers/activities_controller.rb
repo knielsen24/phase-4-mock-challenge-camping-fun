1f5eb0563b098aa2697cefce0e47bffff038e58f
Kevin Nielsen <knielsen24@gmail.com>
2022-07-01 11:08:36 -0700

    2/3

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,18 +1,23 @@
 class ActivitiesController < ApplicationController
+	rescue_from ::ActiveRecord::RecordNotFound, with: :record_not_found
 
 	def index
 		activities = Activity.all
 		render json: activities, status: :ok
 	end
 
-	def delete
+	def destroy
+		activity = Activity.find(params[:id])
+		activity.destroy
+		# render json: { errors: activity.messages}, status: :not_found
+		# record_not_found(activity)
+	end
 
 	private
 
-	def activies_params
-		params.permit(:name, :difficulty)
+	def record_not_found(exception)
+		render json: { error: "Not found" }, status: :not_found
 	end
-
 
 end
 
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,5 +1,4 @@
 class Activity < ApplicationRecord
-	has_many :signups,
-	# dependent :signups
+	has_many :signups, dependent: :destroy
 	has_many :campers, through: :signups
 end
